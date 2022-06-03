0c7d75b66c8c0c0787f648f22a85d9ebe6a358b9
Falahlaz <alfalahlazuardi@gmail.com>
2022-06-03 16:01:07 +0700

    Create repository for notification

---
+++ app/notification/repository/repository.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"github.com/labstack/echo/v4"
+	"gitlab.com/sholludev/sampoerna_notification/models"
+	"gorm.io/gorm"
+)
+
+type Repository interface {
+	GetByIDUser(c echo.Context, DB *gorm.DB, IDUser uint) ([]models.TNotifikasi, error)
+	GetByID(c echo.Context, DB *gorm.DB, ID uint) (models.TNotifikasi, error)
+	Create(c echo.Context, DB *gorm.DB, data *models.TNotifikasi) error
+	UpdateToReadAll(c echo.Context, DB *gorm.DB, IDUser uint) error
+	UpdateToReadSingle(c echo.Context, DB *gorm.DB, data *models.TNotifikasi) error
+}
---
+++ app/notification/repository/repository_impl.go
@@ -0,0 +1,58 @@
+package repository
+
+import (
+	"github.com/labstack/echo/v4"
+	"gitlab.com/sholludev/sampoerna_notification/models"
+	"gorm.io/gorm"
+)
+
+type repositoryImpl struct {
+}
+
+func NewRepository() Repository {
+	return &repositoryImpl{}
+}
+
+// Create implements Repository
+func (*repositoryImpl) Create(c echo.Context, DB *gorm.DB, data *models.TNotifikasi) error {
+	return DB.Create(data).Error
+}
+
+// GetByIDUser implements Repository
+func (*repositoryImpl) GetByID(c echo.Context, DB *gorm.DB, ID uint) (models.TNotifikasi, error) {
+	var data models.TNotifikasi
+	err := DB.Where("id = ?", ID).Where("is_active = true").First(&data).Error
+	return data, err
+}
+
+// GetByID implements Repository
+func (*repositoryImpl) GetByIDUser(c echo.Context, DB *gorm.DB, IDUser uint) ([]models.TNotifikasi, error) {
+	var data []models.TNotifikasi
+	err := DB.Where("id_user = ?", IDUser).Where("is_active = true").Find(&data).Error
+	return data, err
+}
+
+// UpdateToRead implements Repository
+func (*repositoryImpl) UpdateToReadAll(c echo.Context, DB *gorm.DB, IDUser uint) error {
+	var notifikasi []models.TNotifikasi
+	err := DB.Where("id_user = ?", IDUser).Where("is_active = true AND is_read = false").Find(&notifikasi).Error
+	if err != nil {
+		return err
+	}
+
+	for _, v := range notifikasi {
+		v.IsRead = true
+		err = DB.Save(&v).Error
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// UpdateToReadSingle implements Repository
+func (*repositoryImpl) UpdateToReadSingle(c echo.Context, DB *gorm.DB, data *models.TNotifikasi) error {
+	data.IsRead = true
+	return DB.Save(data).Error
+}
